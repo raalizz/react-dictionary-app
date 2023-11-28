b427760ac94f07da2ee0fa6631fe0bd643d44615
raalizz <135476716+raalizz@users.noreply.github.com>
2023-11-28 13:10:09 +0100

    I added Bootstrap and created the main component with the background image and added some styling

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Dictionary</h1>
+    </div>
+  );
+}
