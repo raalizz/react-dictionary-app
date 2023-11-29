ba21212f2f1b0c8c76b3ac9f974ef105a5aa3836
raalizz <135476716+raalizz@users.noreply.github.com>
2023-11-29 11:25:09 +0100

    I changed the layout and added a hero with an image.Also, I changed the name of the App.js to Dictionary.js

---
+++ src/Dictionary.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+
+import "./Dictionary.css";
+
+export default function Dictionary() {
+  return (
+    <div className="Dictionary">
+      <header>
+        <h1>Dictionary</h1>
+      </header>
+    </div>
+  );
+}
