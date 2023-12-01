77c60b5d078313d8ef29fe81ecf77d9ff948b551
raalizz <135476716+raalizz@users.noreply.github.com>
2023-12-01 15:55:18 +0100

    I deleted the SearchBar.js and SearchBar.css and moved everything to the Dictionary.js and Dictionary.css

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,21 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+
 import "bootstrap/dist/css/bootstrap.css";
+
 import Footer from "./Footer";
 import "./Dictionary.css";
-import SearchBar from "./SearchBar";
 import Results from "./Results";
 
 export default function Dictionary() {
+  const [keyword, setKeyword] = useState("");
+  const [results, setResults] = useState(null);
+
+  function Search(event) {
+    event.preventDefault();
+
+    // documentation: https://dictionaryapi.dev/
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleResponse(response) {
+    setResults(response.data[0]);
+  }
+
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
+
   return (
     <div className="Dictionary">
       <header>
         <h6>What word would you like to search?</h6>
-        <SearchBar />
+        <div className="SearchBar">
+          <form onSubmit={Search}>
+            <input
+              type="search"
+              onChange={handleKeywordChange}
+              className="searchEngine"
+              autoFocus={true}
+            />
+          </form>
+        </div>
       </header>
       <main>
         <section className="phonetics">
           <div className="container">
-            <Results />
+            <Results results={results} />
           </div>
         </section>
       </main>
