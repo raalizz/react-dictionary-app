6b7b34122d3a346b4ff89fed99c37595f5ecdecc
raalizz <135476716+raalizz@users.noreply.github.com>
2023-12-04 13:04:58 +0100

    I removed the <br /> from the phonetics component

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -7,8 +7,7 @@ export default function Phonetics(props) {
     <div className="Phonetics">
       <a href={props.phonetics.audio} target="_blank" rel="noopener noreferrer">
         <i className="fa-solid fa-volume-high emoji" title="Listen"></i>
-      </a>
-      <br />
+      </a>{" "}
       {props.phonetics.text}
     </div>
   );
